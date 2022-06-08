53035e1c76bee4dd4b4bf6d637633f2e7ff33a3f
Ilya96s <i.kaltygin@icloud.com>
2022-06-08 22:18:49 +0300

    2.3.6. Проект. Агрегатор Java вакансий 1.1. Job c параметрами

--- src/main/java/ru/job4j/quartz/AlertRabbit.java
+++ src/main/java/ru/job4j/quartz/AlertRabbit.java
@@ -3,7 +3,12 @@ package ru.job4j.quartz;
 import org.quartz.*;
 import org.quartz.impl.StdSchedulerFactory;
 
+import java.io.IOException;
 import java.io.InputStream;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
 import java.util.Properties;
 
 import static org.quartz.JobBuilder.*;
@@ -11,53 +16,94 @@ import static org.quartz.TriggerBuilder.*;
 import static org.quartz.SimpleScheduleBuilder.*;
 
 /**
- * Знакомство с библиотекой Quartz. Выполнение действий с периодичностью.
+ * Выполнение действий с периодичностью. Job с параметрами
  *
  * @author Ilya Kaltygin
- * @version 1.1
+ * @version 1.0
  */
 
 public class AlertRabbit {
     public static void main(String[] args) {
-        try {
+        Properties cfg = getProperties("src/main/resources/rabbit.properties");
+        try (Connection cn = getConnection(cfg)) {
             /* Конфигурирование */
             Scheduler scheduler = StdSchedulerFactory.getDefaultScheduler();
             scheduler.start();
+            /* Дополнительная инфомация для задачи */
+            JobDataMap data = new JobDataMap();
+            data.put("connection", cn);
             /* Создание задачи */
-            JobDetail job = newJob(Rabbit.class).build();
-            /* Создание расписания и настройка периодичности запуска */
+            JobDetail job = newJob(Rabbit.class)
+                    .usingJobData(data)
+                    .build();
+            /* Создание расписания */
             SimpleScheduleBuilder times = simpleSchedule()
-                    .withIntervalInSeconds(Integer.parseInt(
-                            readProperties("rabbit.properties")
-                            .getProperty("rabbit.interval")))
+                    .withIntervalInSeconds(getInterval(cfg))
                     .repeatForever();
-            /* Создание триггера и указание когда должен он должен запуститься */
+            /* Задача выполняется через триггер */
             Trigger trigger = newTrigger()
                     .startNow()
                     .withSchedule(times)
                     .build();
             /* Загрузка задачи и триггера в планировщик */
             scheduler.scheduleJob(job, trigger);
-        } catch (SchedulerException se) {
-            se.printStackTrace();
+            Thread.sleep(5000);
+            scheduler.shutdown();
+        } catch (ClassNotFoundException | SchedulerException | SQLException | InterruptedException e) {
+            e.printStackTrace();
         }
     }
 
-    public static Properties readProperties(String properties) {
+    /**
+     * Загрузка файла конфигурации
+     * @param path имя файла конфигурации
+     * @return Объект Properties
+     */
+    public static Properties getProperties(String path) {
         Properties cfg = new Properties();
-        try (InputStream in = AlertRabbit.class.getClassLoader().getResourceAsStream(properties)) {
+        try (InputStream in = AlertRabbit.class.getClassLoader().getResourceAsStream(path)) {
             cfg.load(in);
-        } catch (Exception e) {
+        } catch (IOException e) {
             e.printStackTrace();
         }
         return cfg;
     }
 
+    /**
+     * Инициализация подключения к базе данных
+     * @param cfg файл с конфигурацией
+     * @return Объект Connection
+     * @throws ClassNotFoundException
+     * @throws SQLException
+     */
+    private static Connection getConnection(Properties cfg) throws SQLException, ClassNotFoundException {
+        Class.forName(cfg.getProperty("jdbc.driver"));
+        return DriverManager.getConnection(
+                cfg.getProperty("jdbc.url"),
+                cfg.getProperty("jdbc.username"),
+                cfg.getProperty("jdbc.password"));
+
+        }
+
+        /**
+     * Метод считывает интервал из файла конфигурации
+     * @param cfg файл с настройкми
+     * @return интервал
+     */
+        public static int getInterval(Properties cfg) {
+        return Integer.parseInt(cfg.getProperty("rabbit.interval"));
+        }
 
     public static class Rabbit implements Job {
         @Override
         public void execute(JobExecutionContext context) throws JobExecutionException {
-            System.out.println("Rabbit runs here ...");
+            Connection cn = (Connection) context.getJobDetail().getJobDataMap().get("connection");
+            try (PreparedStatement statement = cn.prepareStatement("insert into rabbit(created_date) values(?);")) {
+                statement.setLong(1, System.currentTimeMillis());
+                statement.execute();
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
         }
     }
 }
