754a55ea29219e94ccbf0a7f52228c48ab90ef7d
Ilya96s <i.kaltygin@icloud.com>
2022-06-16 22:06:06 +0300

    2.3.6. Проект. Агрегатор Java вакансий 5. PsqlStore

---
+++ src/main/java/ru/job4j/grabber/Grabber.java
@@ -0,0 +1,124 @@
+package ru.job4j.grabber;
+
+import org.quartz.*;
+import org.quartz.impl.StdSchedulerFactory;
+import ru.job4j.grabber.utils.HabrCareerDateTimeParser;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.List;
+import java.util.Properties;
+
+import static org.quartz.JobBuilder.newJob;
+import static org.quartz.SimpleScheduleBuilder.simpleSchedule;
+import static org.quartz.TriggerBuilder.newTrigger;
+
+/**
+ * Соединение всех частей программы в целое приложение
+ *
+ * @author Ilya Kaltygin
+ */
+public class Grabber implements Grab {
+
+    private final Properties cfg = new Properties();
+
+    private static final String PAGE_LINK = "https://career.habr.com/vacancies/java_developer?page=";
+
+    /**
+     * Метоод возвращает объекта типа Store
+     */
+    public Store store() {
+        return new PsqlStore(cfg);
+    }
+
+    /**
+     * Создание планировщика и его запуск
+     * @return планировщик
+     */
+    public Scheduler scheduler() {
+        Scheduler scheduler = null;
+        try {
+            scheduler = StdSchedulerFactory.getDefaultScheduler();
+            scheduler.start();
+        } catch (SchedulerException e) {
+            e.printStackTrace();
+        }
+        return scheduler;
+    }
+
+    /**
+     * Загрузка настроек
+     */
+    public void cfg() {
+        try (InputStream in = Grab.class.getClassLoader().getResourceAsStream("grabber.properties")) {
+            cfg.load(in);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     * Периодический запуск парсера с использованием quartz
+     * @param parse объект типа Parse
+     * @param store объект типа Store
+     * @param scheduler объект типа Scheduler
+     */
+    @Override
+    public void init(Parse parse, Store store, Scheduler scheduler) {
+        try {
+            /* Передача информации в задачу, интерфейсы Parse и Store нужны для выполнения парсинга и подключения к бд */
+            JobDataMap data = new JobDataMap();
+            data.put("store", store);
+            data.put("parse", parse);
+            /* Создание задачи */
+            JobDetail job = newJob(GrabJob.class)
+                    .usingJobData(data)
+                    .build();
+            /* Расписание выполнения задачи */
+            SimpleScheduleBuilder times = simpleSchedule()
+                    .withIntervalInSeconds(Integer.parseInt(cfg.getProperty("time")))
+                    .repeatForever();
+            /* В триггере указывю расписание и когда начинать запуск */
+            Trigger trigger = newTrigger()
+                    .startNow()
+                    .withSchedule(times)
+                    .build();
+            /* Передача триггера и задачи в планировщик */
+            scheduler.scheduleJob(job, trigger);
+        } catch (SchedulerException e) {
+            e.printStackTrace();
+        }
+    }
+
+
+    public static class GrabJob implements Job {
+
+        /**
+         * Действия, которые будет выполнять планировщик
+         * @param context объект содержит информацию, которая была передана заданию, перед тем как оно было запущено
+         * @throws JobExecutionException
+         */
+        @Override
+        public void execute(JobExecutionContext context) throws JobExecutionException {
+            /* Извлечение информации, которая была передана с помощью JobDataMap в методе init */
+            JobDataMap map = context.getJobDetail().getJobDataMap();
+            Store store = (Store) map.get("store");
+            Parse parse = (Parse) map.get("parse");
+            List<Post> postList = parse.list(PAGE_LINK);
+            postList.forEach(store::save);
+            List<Post> getPostList = store.getAll();
+            for (Post post : getPostList) {
+                System.out.println(post);
+            }
+            System.out.println(store.findById(20));
+        }
+    }
+
+    public static void main(String[] args) {
+        Grabber grabber = new Grabber();
+        grabber.cfg();
+        Scheduler scheduler = grabber.scheduler();
+        Store store = grabber.store();
+        grabber.init(new HabrCareerParse(new HabrCareerDateTimeParser()), store, scheduler);
+    }
+}
