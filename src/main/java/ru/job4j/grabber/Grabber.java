4005ae4284d52e4e74ae6319a34d8ad7f26c8151
Ilya96s <i.kaltygin@icloud.com>
2022-06-17 21:55:00 +0300

    2.3.6. Проект. Агрегатор Java вакансий 7. Web

--- src/main/java/ru/job4j/grabber/Grabber.java
+++ src/main/java/ru/job4j/grabber/Grabber.java
@@ -6,6 +6,10 @@ import ru.job4j.grabber.utils.HabrCareerDateTimeParser;
 
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.nio.charset.Charset;
 import java.util.List;
 import java.util.Properties;
 
@@ -90,6 +94,30 @@ public class Grabber implements Grab {
         }
     }
 
+    /**
+     * Метод создает серверный сокет и при отправляет клиенту содержимое базы данных
+     * @param store объект определяет связь с базой данных
+     */
+    public void web(Store store) {
+        new Thread(() -> {
+            try (ServerSocket server = new ServerSocket(Integer.parseInt(cfg.getProperty("port")))) {
+                while (!server.isClosed()) {
+                    Socket socket = server.accept();
+                    try (OutputStream out = socket.getOutputStream()) {
+                        out.write("HTTP/1.1 200 OK\r\n\r\n".getBytes());
+                        for (Post post : store.getAll()) {
+                            out.write(post.toString().getBytes(Charset.forName("Windows-1251")));
+                            out.write(System.lineSeparator().getBytes());
+                        }
+                    } catch (IOException io) {
+                        io.printStackTrace();
+                    }
+                }
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }).start();
+    }
 
     public static class GrabJob implements Job {
 
@@ -120,5 +148,6 @@ public class Grabber implements Grab {
         Scheduler scheduler = grabber.scheduler();
         Store store = grabber.store();
         grabber.init(new HabrCareerParse(new HabrCareerDateTimeParser()), store, scheduler);
+        grabber.web(store);
     }
 }
